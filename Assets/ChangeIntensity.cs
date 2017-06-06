9bcc7c2ab00cee242043c45014f7cbd30eed24d3
Tom Wall <twall@millennialmedia.com>
2017-06-06 00:15:33 -0400

    first

---
+++ Assets/ChangeIntensity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeIntensity : MonoBehaviour {
+	public float offset;
+	// Update is called once per frame
+	void Update () {
+		if ((Time.fixedTime + offset) % 10 > 5)
+			gameObject.GetComponent<TubeLight> ().m_Intensity = (float)(0.8 * Mathf.Abs (Mathf.Sin ((Time.fixedTime + offset) * 10)));
+		else
+			gameObject.GetComponent<TubeLight> ().m_Intensity = (float)0.8;
+	}
+}
---
+++ Assets/changeGlow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+[RequireComponent(typeof(PostProcessingBehaviour))]
+public class changeGlow : MonoBehaviour
+{
+	PostProcessingProfile m_Profile;
+
+	void OnEnable()
+	{
+		var behaviour = GetComponent<PostProcessingBehaviour>();
+
+		if (behaviour.profile == null)
+		{
+			enabled = false;
+			return;
+		}
+
+		m_Profile = Instantiate(behaviour.profile);
+		behaviour.profile = m_Profile;
+	}
+
+	void Update()
+	{
+		var bloom = m_Profile.bloom.settings;
+		//bloom.bloom.intensity = 2 + Mathf.Sin (Time.fixedTime) * 2;
+		m_Profile.bloom.settings = bloom;
+	}
+}
