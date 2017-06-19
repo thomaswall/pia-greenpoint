e81c9740f9721b1da745c0eafb8a7c2fe05ae5b3
Tom Wall <twall@millennialmedia.com>
2017-06-18 22:59:32 -0400

    new scene and ws

--- Assets/ChangeIntensity.cs
+++ Assets/ChangeIntensity.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ChangeIntensity : MonoBehaviour {
 	public float offset;
+	float trigger = 0;
 	// Update is called once per frame
+	IEnumerator Start () {
+		WebSocket w = new WebSocket(new Uri("ws://127.0.0.1:1337"));
+		yield return StartCoroutine(w.Connect());
+		w.SendString("Hi there");
+		int i=0;
+		while (true)
+		{
+			string reply = w.RecvString ();
+			if (reply != null)
+			{
+				Debug.Log ("Received: "+reply);
+				trigger = Time.fixedTime;
+			}
+			if (w.error != null)
+			{
+				Debug.LogError ("Error: "+w.error);
+				break;
+			}
+			yield return 0;
+		}
+		w.Close();
+	}
+
 	void Update () {
-		if ((Time.fixedTime + offset) % 10 > 5)
+		float time_since_trigger = Time.fixedTime - (trigger + offset);
+		if (time_since_trigger > 0 && time_since_trigger < 5)
 			gameObject.GetComponent<TubeLight> ().m_Intensity = (float)(0.8 * Mathf.Abs (Mathf.Sin ((Time.fixedTime + offset) * 10)));
-		else
+		else if (time_since_trigger < 10)
 			gameObject.GetComponent<TubeLight> ().m_Intensity = (float)0.8;
+		else
+			gameObject.GetComponent<TubeLight> ().m_Intensity = 0;
 	}
 }
---
+++ Assets/lightsOn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightsOn : MonoBehaviour {
+	private Color color;
+	public float offset;
+
+	void Start () {
+		color = GetComponent<Renderer>().material.color;
+	}
+
+	void Update () {
+		var material = GetComponent<Renderer> ().material;
+
+
+		if ((Time.fixedTime) % 6.0 >= offset && (Time.fixedTime) % 6.0 < offset + 2) {
+			material.color = color;
+		} else {
+			material.color = new Color (color.r, color.g, color.b, 0);
+		}
+	}
+}
